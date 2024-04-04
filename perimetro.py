c9f5bb9131e8bdd864082a01db326b4ebc5ca94d
fhasbf <166052907+fhasbf@users.noreply.github.com>
2024-04-04 16:53:48 -0300

    Add files via upload

---
+++ perimetro.py
@@ -0,0 +1,5 @@
+print ("bem vindo")
+largura = float (input  ("digite a largura"))
+altura = float (input ('digite a altura'))
+perimetro = ( largura + altura) *2
+print ("seu perimetro é" , perimetro)
