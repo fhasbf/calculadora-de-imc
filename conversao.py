235989261dc4f460a98da2d3fccfc938def3ac85
fhasbf <166052907+fhasbf@users.noreply.github.com>
2024-04-09 17:00:12 -0300

    Add files via upload

---
+++ conversao.py
@@ -0,0 +1,19 @@
+print ('bem vindo')
+valor = float (input ("digite o número que deseja converter"))
+sist= int (input ('digite sua medida, 1 para milhas, 2 polegadas, 3 pés, 4 centímetros, 5 metros, 6 quilometros)'))
+# valor é o numero ou distancia
+#sist é o sistema de medida
+if sist == 1 :
+    polegadas = (valor * 63360)
+    pes = (valor * 5280)
+    centimetros = (valor * 160934.4)
+    metros = (valor * 1609.3)
+    quilometros = (valor * 1.60934)
+    print ("isso dá polegadas" ,polegadas, "pés,", pes, "centímetros,", centimetros, "metros,", metros, "quilometros", quilometros, ".")
+elif sist == 2:
+    milhas = 
+    pés =
+    centimetros = 
+    metros = 
+    quilometros =
+    print ("isso são")
