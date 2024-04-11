63e506e61c6de75c0281b165a7bfc5a0c5b11607
fhasbf <166052907+fhasbf@users.noreply.github.com>
2024-04-11 16:45:41 -0300

    Update juros.py

--- juros.py
+++ juros.py
@@ -1,6 +1,6 @@
-print ("bem vindo, vamos calcular os juros")
+print  ("bem vindo, vamos calcular os juros")
 valor = float (input ("digite o valor da divida"))
 juros = float (input ("digite a taxa de juros"))
 periodo = float (input ("digite o periodo que levara para quitar a divida"))
-valor_total = ( valor * juros * periodo)
+valor_total =  valor * (1 + juros) ** periodo
 print ("o valor total fica em", valor_total)
